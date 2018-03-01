1060e64a6cc8fba990d6dce92214e00ce0a1a40a
liugh22 <guanhua.liu@midea.com>
2018-03-01 19:13:53 +0800

    go

---
+++ src/main/java/com/lgh/springboot/graylog2/demo/SpringbootGraylog2DemoApplication.java
@@ -0,0 +1,12 @@
+package com.lgh.springboot.graylog2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraylog2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGraylog2DemoApplication.class, args);
+	}
+}
